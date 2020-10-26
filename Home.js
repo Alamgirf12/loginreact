64869aca78dfb25aaff3be3417964cd8f1c19dbf
Alamgirf12 <60141800+Alamgirf12@users.noreply.github.com>
2020-10-26 11:23:48 +0600

    Add files via upload
    add react

---
+++ Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
+
+
+const Home = {} =>{
+	return(
+     <div className="container">
+     	<div className="py-4">
+     		Home Page
+     	</div>	
+     </div>
+		)
+}
+export default Home;
